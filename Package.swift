5861ddec134f807a03ed00ea7d9bb5b246abd71f
xkakjd88178 <154839794+xkakjd88178@users.noreply.github.com>
2024-12-25 00:44:36 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SwiftfulUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftfulThinking/SwiftfulRecursiveUI.git", from: "1.0.0")
+        .package(url: "https://github.com/xkakjd88178/SwiftfulRecursiveUI.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
